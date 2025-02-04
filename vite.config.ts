dec9c71c271eee09222c0117f2c7829c68d44c58
pktjesus1 <pktjesus@gmail.com>
2025-02-04 20:31:09 +0900

    dev dns추가1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['dev.gidosa.net'] // 여기에 호스트 추가!
+  },
 })
